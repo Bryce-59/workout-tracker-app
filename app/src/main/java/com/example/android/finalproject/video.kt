f7b470acef74adb61859f3b5816b89c07e137f97
David He <ameer886@outlook.com>
2022-11-13 20:20:15 -0800

    added back navigation to video activity

--- app/src/main/java/com/example/android/finalproject/video.kt
+++ app/src/main/java/com/example/android/finalproject/video.kt
@@ -2,10 +2,18 @@ package com.example.android.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class video : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(R.layout.activity_video)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        finish()
+        return super.onOptionsItemSelected(item)
     }
 }

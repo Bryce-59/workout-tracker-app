8b3b89a056a4938dd008baaf31367b8f5e93e68e
David He <ameer886@outlook.com>
2022-11-28 15:57:27 -0800

    clean unused imports

--- app/src/main/java/com/example/android/finalproject/ui/UserFragment.kt
+++ app/src/main/java/com/example/android/finalproject/ui/UserFragment.kt
@@ -1,14 +1,10 @@
 package com.example.android.finalproject.ui
 
-import android.app.AlertDialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
